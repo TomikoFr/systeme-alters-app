437304b259f82877909eaf4b0e0cb598a5301a5e
megam <megam@local>
2026-06-29 15:20:51 +0200

    Configure Supabase publishable key

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.PLURAL_HOME_SUPABASE = {
   url: "https://qypuxsaycserysutqhty.supabase.co",
-  anonKey: "COLLE_ICI_LA_CLE_PUBLIQUE_ANON_OU_PUBLISHABLE"
+  anonKey: "sb_publishable_fjaREfJu0Y9rFjKkePYOkQ_rAZH3HPa"
 };
